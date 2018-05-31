64b651a9e74ea5d8ac33168b9609304322429cbc
Luca Becatti <luca.bbecatti@gmail.com>
2018-05-31 16:55:56 +0200

    Commit 3

--- app/Parere.php
+++ app/Parere.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Parere extends Model
 {
-	//
+	
 	protected $table = 'bdu_commissione_pareri';
-	protected $primaryKey = 'id_commissionse_pareri';
+	protected $primaryKey = 'id_commissione_pareri';
 	public $timestamps = false;
     //
 }
